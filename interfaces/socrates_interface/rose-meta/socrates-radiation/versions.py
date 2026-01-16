ab7e7543540ddf08d3a451c8f1bf583854eda035
James Manners <james.manners@metoffice.gov.uk>
2026-01-16 14:04:46 +0000

    #135: Working upgrade macro

--- interfaces/socrates_interface/rose-meta/socrates-radiation/versions.py
+++ interfaces/socrates_interface/rose-meta/socrates-radiation/versions.py
@@ -18,19 +18,6 @@ class UpgradeError(Exception):
     __str__ = __repr__
 
 
-class vnXX_txxx(MacroUpgrade):
-    # Upgrade macro for #135 by James Manners
-
-    BEFORE_TAG = "vn3.0"
-    AFTER_TAG = "vn3.0_t135"
-
-    def upgrade(self, config, meta_config=None):
-        self.add_setting(
-            config, ["namelist:cosp", "n_cosp_step"], "1"
-        )
-        return config, self.reports
-
-
 """
 Copy this template and complete to add your macro
 
@@ -44,3 +31,14 @@ class vnXX_txxx(MacroUpgrade):
         # Add settings
         return config, self.reports
 """
+
+
+class vn30_t135(MacroUpgrade):
+    # Upgrade macro for #135 by James Manners
+
+    BEFORE_TAG = "vn3.0"
+    AFTER_TAG = "vn3.0_t135"
+
+    def upgrade(self, config, meta_config=None):
+        self.add_setting(config, ["namelist:cosp", "n_cosp_step"], "1")
+        return config, self.reports
