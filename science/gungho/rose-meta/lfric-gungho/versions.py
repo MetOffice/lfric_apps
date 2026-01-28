c5b553f95da47b0aae47560e9301919b9f7c6177
cameronbateman-mo <cameron.bateman@metoffice.gov.uk>
2026-01-28 15:10:38 +0000

    Revert "updated macros"
    This reverts commit 4a447a4342a142de6dd3dd0006daa53980084f9b.

--- science/gungho/rose-meta/lfric-gungho/versions.py
+++ science/gungho/rose-meta/lfric-gungho/versions.py
@@ -4,6 +4,7 @@ from metomi.rose.upgrade import MacroUpgrade
 
 from .version22_30 import *
 
+
 class UpgradeError(Exception):
     """Exception created when an upgrade fails."""
 
@@ -12,7 +13,7 @@ class UpgradeError(Exception):
 
     def __repr__(self):
         sys.tracebacklimit = 0
-        return self.msgno 
+        return self.msg
 
     __str__ = __repr__
 
@@ -26,19 +27,18 @@ class vn30_t99(MacroUpgrade):
         # Commands From: rose-meta/lfric-lfric_atm
         """Set segmentation size for Gregory-Rowntree convection kernel"""
         self.add_setting(config, ["namelist:physics", "conv_gr_segment"], "16")
-        return config, self.reports
 
+        return config, self.reports
+      
 class vn30_t171(MacroUpgrade):
-    """Upgrade macro for ticket #171 by James Kent."""
+    """Upgrade macro for #171 by James Kent."""
 
     BEFORE_TAG = "vn3.0_t99"
     AFTER_TAG = "vn3.0_t171"
 
     def upgrade(self, config, meta_config=None):
-        # Commands From: rose-meta/lfric-gungho
         # Add adjust_tracer_equation to transport namelist
         self.add_setting(
             config, ["namelist:transport", "adjust_tracer_equation"], ".false."
         )
-
         return config, self.reports
