027dd1f5e96bf3d2669dfdbb180a2f1c1400bfaf
DrTVockerodtMO <114994380+DrTVockerodtMO@users.noreply.github.com>
2026-03-09 11:31:48 +0000

    Fixed faulty macro by manually upgrading adjoint_tests config

--- science/adjoint/rose-meta/lfric-adjoint/versions.py
+++ science/adjoint/rose-meta/lfric-adjoint/versions.py
@@ -51,32 +51,27 @@ class vn31_t322(MacroUpgrade):
         exec_name = self.get_setting_value(
             config, ["env", "EXEC_NAME"]
         )
-        if exec_name in exception_exec_names :
-          source = re.sub(
-              r"namelist:base_mesh",
-              r"namelist:adjoint" + "\n" + "  namelist:base_mesh",
-              source,
-          )
-        else:
-          source = re.sub(
-              r".namelist:aerosol.",
-              r" namelist:adjoint" + "\n" + " (namelist:aerosol)",
-              source,
-          )
+        # To prevent macro upgrade errors, we edit the adjoint_tests config manually
+        if exec_name != "adjoint_tests":
+          if exec_name in exception_exec_names :
+            source = re.sub(
+                r"namelist:base_mesh",
+                r"namelist:adjoint" + "\n" + "  namelist:base_mesh",
+                source,
+            )
+          else:
+            source = re.sub(
+                r".namelist:aerosol.",
+                r" namelist:adjoint" + "\n" + " (namelist:aerosol)",
+                source,
+            )
         self.change_setting_value(
             config, ["file:configuration.nml", "source"], source
         )
 
-        # We don't want to directly modify optional configs
-        if exec_name != "${APP_NAME}":
-          # Set computation of annexed dofs to true for adjoint, false for adjoint_tests
-          if exec_name == "adjoint_tests":
-            self.add_setting(
-                config, ["namelist:adjoint", "l_compute_annexed_dofs"], ".false."
-            )
-          else:
-            self.add_setting(
-                config, ["namelist:adjoint", "l_compute_annexed_dofs"], ".true."
-            )
+        # Default value
+        self.add_setting(
+            config, ["namelist:adjoint", "l_compute_annexed_dofs"], ".true."
+        )
 
         return config, self.reports
