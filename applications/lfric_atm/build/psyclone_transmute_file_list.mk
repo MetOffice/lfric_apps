de30039478fb3ad663353b9362d991bb83df9d4c
MetBenjaminWent <benjamin.went@metoffice.gov.uk>
2026-03-17 15:32:21 +0000

    Correct makefile

--- applications/lfric_atm/build/psyclone_transmute_file_list.mk
+++ applications/lfric_atm/build/psyclone_transmute_file_list.mk
@@ -18,7 +18,7 @@ export PSYCLONE_PHYSICS_FILES = \
                                 bl_diags_mod \
                                 bl_exp_kernel_mod \
                                 bl_imp_kernel_mod \
-                                bl_imp2_kernel_mod 
+                                bl_imp2_kernel_mod \
                                 bl_lsp \
                                 bm_tau_kernel_mod \
                                 btq_int \
