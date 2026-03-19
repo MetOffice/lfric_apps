dbfa35c473fa9b6af6eb61c3c9cb5ab91a2aebe1
stevemullerworth <26165365+stevemullerworth@users.noreply.github.com>
2026-03-19 17:00:15 +0000

    Upgrade macro application

--- science/adjoint/rose-meta/lfric-adjoint/versions.py
+++ science/adjoint/rose-meta/lfric-adjoint/versions.py
@@ -19,21 +19,6 @@ class UpgradeError(Exception):
     __str__ = __repr__
 
 
-"""
-Copy this template and complete to add your macro
-
-class vnXX_txxx(MacroUpgrade):
-    # Upgrade macro for <TICKET> by <Author>
-
-    BEFORE_TAG = "vnX.X"
-    AFTER_TAG = "vnX.X_txxx"
-
-    def upgrade(self, config, meta_config=None):
-        # Add settings
-        return config, self.reports
-"""
-
-
 class vn31_t322(MacroUpgrade):
     """Upgrade macro for ticket #322 by Terence Vockerodt."""
 
@@ -41,6 +26,7 @@ class vn31_t322(MacroUpgrade):
     AFTER_TAG = "vn3.1_t322"
 
     def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-adjoint
         # Adds new namelist entry alphabetically
         source = self.get_setting_value(
             config, ["file:configuration.nml", "source"]
