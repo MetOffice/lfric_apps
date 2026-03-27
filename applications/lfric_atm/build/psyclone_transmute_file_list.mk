7769608c991a1a792ee08a995ec4f4d94849f717
Rob Waters <rw796@cam.ac.uk>
2026-03-27 09:52:04 +0000

    PSYCLONE_PASS_NO_SCRIPTS mistakenly removed during rebase

--- applications/lfric_atm/build/psyclone_transmute_file_list.mk
+++ applications/lfric_atm/build/psyclone_transmute_file_list.mk
@@ -46,6 +46,14 @@ export PSYCLONE_PHYSICS_FILES = \
                                 jules_exp_kernel_mod \
                                 jules_extra_kernel_mod
 
+##### TRANSMUTE_INCLUDE_METHOD specify_include #####
+
+# List to use PSyclone explicitly without any opt script
+# This will remove hand written (OMP) directives in the source
+# Used by both methods, specify_include and specify_exclude
+export PSYCLONE_PASS_NO_SCRIPT = ukca_abdulrazzak_ghan
+
+
 ##### TRANSMUTE_INCLUDE_METHOD specify_exclude #####
 # For GPU, we may want to use more generic local.py transformation scripts and psyclone by directory.
 # Advise which directories to pass to PSyclone.
