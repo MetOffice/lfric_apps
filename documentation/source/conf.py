1d3d134eb30528bff6ec1f61d109d378f84c744a
MichaelWhitall <43407209+MichaelWhitall@users.noreply.github.com>
2026-04-02 15:39:59 +0100

    Reverted unneeded changes to conf.py

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -20,8 +20,7 @@ release = '0.1.0'
 extensions = [
     'sphinx_sitemap',
     'sphinx_design',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autosectionlabel'
+    'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -96,9 +95,6 @@ html_context = {
 # Enable numbered references to e.g. figures.
 #
 numfig = True
-html_use_modindex = True
-autosectionlabel_prefix_document = True
-autosectionlabel_maxdepth = 3
 
 # Exclude files from Sphinx processing
 exclude_patterns = ['common_links.rst']
