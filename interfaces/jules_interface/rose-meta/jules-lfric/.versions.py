3f0ce0d16336faad5aed58f31b5b58c786e7ade2
Dan Copsey <dan.copsey@metoffice.gov.uk>
2026-04-10 16:55:16 +0100

    Improve command to help black

--- interfaces/jules_interface/rose-meta/jules-lfric/.versions.py
+++ interfaces/jules_interface/rose-meta/jules-lfric/.versions.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 from metomi.rose.upgrade import MacroUpgrade  # noqa: F401
@@ -20,19 +21,14 @@ class UpgradeError(Exception):
 
 """
 Copy this template and complete to add your macro
-
 class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
-
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
-
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
 """
-
-
 class vn31_t400(MacroUpgrade):
     """Upgrade macro for ticket #400 by Dan Copsey."""
 
@@ -40,11 +36,10 @@ class vn31_t400(MacroUpgrade):
     AFTER_TAG = "vn3.1_t400"
 
     def upgrade(self, config, meta_config=None):
-
+        # Commands From: rose-meta/jules-lfric
         # Is this a coupled model? The easiest way to get this is from the number of sea ice categories.
         nice = int(self.get_setting_value(config, ["namelist:jules_sea_seaice", "nice"])
-        is_coupled = bool(nice > 1)
- 
+        is_coupled = nice > 1
         # Add the new namelist settings
         self.add_setting(config, ["namelist:jules_sea_seaice", "l_zenith_albedo"], ".false.")
         if is_coupled:
@@ -54,5 +49,6 @@ class vn31_t400(MacroUpgrade):
         self.add_setting(config, ["namelist:jules_sea_seaice", "snow_grain_size_max"], "100.0")
         self.add_setting(config, ["namelist:jules_sea_seaice", "snow_grain_size_min"], "70.0")
         self.add_setting(config, ["namelist:jules_sea_seaice", "snowpatch"], "0.02")
+
 
         return config, self.reports
