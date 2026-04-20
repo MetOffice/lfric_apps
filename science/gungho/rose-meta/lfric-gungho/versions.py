fddafe06bcde71578f8266d9222977b7bface5ba
Matthew Hambley <MatthewHambley@users.noreply.github.com>
2026-04-20 08:24:05 +0100

    Further mistakes in conflict resolution.

--- science/gungho/rose-meta/lfric-gungho/versions.py
+++ science/gungho/rose-meta/lfric-gungho/versions.py
@@ -25,6 +25,10 @@ class vnXX_txxx(MacroUpgrade):
     # Upgrade macro for <TICKET> by <Author>
     BEFORE_TAG = "vnX.X"
     AFTER_TAG = "vnX.X_txxx"
+
+    def upgrade(self, config, meta_config=None):
+        # Add settings
+        return config, self.reports
 """
 
 
