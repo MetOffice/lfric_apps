8a9e41f942f333c83f0134f63ab57a324b2eb635
mo-marqh <87382490+mo-marqh@users.noreply.github.com>
2026-04-28 16:27:53 +0000

    further test cases

--- rose-stem/app/validate_output_file_metadata/bin/test_validate_metadata.py
+++ rose-stem/app/validate_output_file_metadata/bin/test_validate_metadata.py
@@ -17,14 +17,52 @@ def load_rootgrp(infile):
     print(f"loading ... ... {infile}")
     return rootgrp
 
-# def test_time_instant_exists(load_rootgrp):
-#     "validate that the output file has a variable named 'time_instant'"
-#     assert "time_instant" in load_rootgrp.variables
+class TestTemporalMetadata:
+    def test_time_centered_exists(self, load_rootgrp):
+        """
+        Validate that the output file has a variable named 'time_instant'
+        """
+        assert "time_centered" in load_rootgrp.variables
 
-def test_time_centered_exists(load_rootgrp):
-    "validate that the output file has a variable named 'time_instant'"
-    assert "time_centered" in load_rootgrp.variables
+    def test_time_counter_not_exists(self, load_rootgrp):
+        """
+        Validate that the output file does not have a variable named 'time_counter'
+        """
+        assert "time_counter" not in load_rootgrp.variables
 
-def test_time_counter_not_exists(load_rootgrp):
-    "validate that the output file does not have a variable named 'time_counter'"
-    assert "time_counter" not in load_rootgrp.variables
+class TestSpatialMetadata:
+    def test_Mesh2d_node_x_exists(self, load_rootgrp):
+        """
+        Validate that the output file has a variable named 'Mesh2d_node_x'
+        """
+        assert "Mesh2d_node_x" in load_rootgrp.variables
+
+    def test_Mesh2d_node_x_stdname(self, load_rootgrp):
+        """
+        Validate that the output file has a variable named 'Mesh2d_node_x'
+        """
+        assert load_rootgrp.variables.get("Mesh2d_node_x").standard_name == 'longitude'
+
+    def test_Mesh2d_node_x_units(self, load_rootgrp):
+        """
+        Validate that the output file has a variable named 'Mesh2d_node_x'
+        """
+        assert load_rootgrp.variables.get("Mesh2d_node_x").units == 'degrees_east'
+
+    def test_Mesh2d_node_y_exists(self, load_rootgrp):
+        """
+        Validate that the output file has a variable named 'Mesh2d_node_y'
+        """
+        assert "Mesh2d_node_y" in load_rootgrp.variables
+
+    def test_Mesh2d_node_y_stdname(self, load_rootgrp):
+        """
+        Validate that the output file has a variable named 'Mesh2d_node_y'
+        """
+        assert load_rootgrp.variables.get("Mesh2d_node_y").standard_name == 'latitude'
+
+    def test_Mesh2d_node_y_units(self, load_rootgrp):
+        """
+        Validate that the output file has a variable named 'Mesh2d_node_y'
+        """
+        assert load_rootgrp.variables.get("Mesh2d_node_y").units == 'degrees_north'
