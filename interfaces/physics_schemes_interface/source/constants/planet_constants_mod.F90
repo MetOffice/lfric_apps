4760ec75c1f674ea33049eab0afb4a398ebbb797
bblay <byron.blay@metoffice.gov.uk>
2026-06-09 16:39:13 +0100

    review actions wip

--- interfaces/physics_schemes_interface/source/constants/planet_constants_mod.F90
+++ interfaces/physics_schemes_interface/source/constants/planet_constants_mod.F90
@@ -26,7 +26,7 @@ module planet_constants_mod
             c_virtual_bl, etar_bl, repsilon_bl, ls_bl, r_32b, c_virtual_32b,  &
             etar_32b, lcrcp_32b, ls_32b, lsrcp_32b, planet_radius_bl,         &
             recip_kappa_bl, power, ex_power, lcrcp_def, lsrcp_def,            &
-            recip_kappa_def
+            recip_kappa_def, g_over_r
 
   ! The following variables have been hidden as they are not currently
   ! required to build the extracted UM code. They have been left in
@@ -36,7 +36,6 @@ module planet_constants_mod
 
   ! Disabled variables:
   !   sclht, omega, two_omega, recip_p_zero,
-  !   g_over_r
 
 
 !----------------------------------------------------------------------
