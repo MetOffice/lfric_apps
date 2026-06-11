d8ea3058803bb5500a3afc50b7d6adcbf8a29ac3
Chris Smith <256821015+mo-cjsmith@users.noreply.github.com>
2026-06-11 16:37:29 +0100

    Improved comments

--- science/gungho/source/kernel/initialisation/regrav_preserve_t_kernel_mod.F90
+++ science/gungho/source/kernel/initialisation/regrav_preserve_t_kernel_mod.F90
@@ -4,7 +4,7 @@
 ! under which the code may be used.
 !-----------------------------------------------------------------------------
 
-!> @brief   Defines exner pressure from vertical balance with a deep gravity,
+!> @brief   Calculates exner pressure from vertical balance with a deep gravity,
 !>          but using input data based on a shallow gravity.
 !> @details Using the input exner pressure at level eos_index, integrate down
 !>          to the surface using hydrostatic balance, plus Coriolis terms,
@@ -14,14 +14,14 @@
 !>          or deep depending on configuration).
 module regrav_preserve_t_kernel_mod
 
-use argument_mod,               only : arg_type,                 &
-                                       GH_FIELD, GH_REAL,        &
-                                       GH_SCALAR, GH_INTEGER,    &
-                                       GH_READ, GH_READWRITE,    &
-                                       CELL_COLUMN
-use constants_mod,              only : r_def, i_def
-use fs_continuity_mod,          only : Wtheta, W3
-use kernel_mod,                 only : kernel_type
+use argument_mod,               only: arg_type,              &
+                                      GH_FIELD, GH_REAL,     &
+                                      GH_SCALAR, GH_INTEGER, &
+                                      GH_READ, GH_READWRITE, &
+                                      CELL_COLUMN
+use constants_mod,              only: r_def, i_def
+use fs_continuity_mod,          only: Wtheta, W3
+use kernel_mod,                 only: kernel_type
 
 implicit none
 
@@ -32,17 +32,17 @@ private
 !-------------------------------------------------------------------------------
 type, public, extends(kernel_type) :: regrav_preserve_t_kernel_type
   private
-  type(arg_type) :: meta_args(10) = (/                      &
-       arg_type(GH_FIELD,  GH_REAL, GH_READWRITE, W3),      &
-       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta),  &
-       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta),  &
-       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta),  &
-       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta),  &
-       arg_type(GH_FIELD,  GH_REAL, GH_READ,      W3),      &
-       arg_type(GH_FIELD,  GH_REAL, GH_READ,      W3),      &
-       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta),  &
-       arg_type(GH_FIELD,  GH_REAL, GH_READ,      W3),      &
-       arg_type(GH_SCALAR, GH_REAL, GH_READ)                &
+  type(arg_type) :: meta_args(10) = (/                     &
+       arg_type(GH_FIELD,  GH_REAL, GH_READWRITE, W3),     &
+       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta), &
+       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta), &
+       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta), &
+       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta), &
+       arg_type(GH_FIELD,  GH_REAL, GH_READ,      W3),     &
+       arg_type(GH_FIELD,  GH_REAL, GH_READ,      W3),     &
+       arg_type(GH_FIELD,  GH_REAL, GH_READ,      Wtheta), &
+       arg_type(GH_FIELD,  GH_REAL, GH_READ,      W3),     &
+       arg_type(GH_SCALAR, GH_REAL, GH_READ)               &
        /)
   integer :: operates_on = CELL_COLUMN
 contains
@@ -65,7 +65,7 @@ contains
 !> @param[in]  phi           Geopotential field
 !> @param[in]  height_w3     Height coordinate in w3
 !> @param[in]  height_wth    Height coordinate in wth
-!> @param[in]  w3_mask       LBC mask or Dummy mask for w3 space
+!> @param[in]  mask_w3       LBC mask or Dummy mask for w3 space
 !> @param[in]  cp            Specific heat capacity at constant pressure
 !> @param[in]  ndf_w3        Number of degrees of freedom per cell for w3
 !> @param[in]  undf_w3       Total number of degrees of freedom for w3
@@ -82,7 +82,7 @@ subroutine regrav_preserve_t_code( nlayers,       &
                                    phi,           &
                                    height_w3,     &
                                    height_wth,    &
-                                   w3_mask,       &
+                                   mask_w3,       &
                                    cp,            &
                                    ndf_w3,        &
                                    undf_w3,       &
@@ -94,25 +94,24 @@ subroutine regrav_preserve_t_code( nlayers,       &
   implicit none
 
   ! Arguments
+  integer(kind=i_def), intent(in) :: nlayers, &
+                                     ndf_w3,  &
+                                     undf_w3, &
+                                     ndf_wt,  &
+                                     undf_wt
 
-  integer(kind=i_def),                    intent(in) :: nlayers, &
-                                                        ndf_w3,  &
-                                                        undf_w3, &
-                                                        ndf_wt,  &
-                                                        undf_wt
-  integer(kind=i_def), dimension(ndf_w3), intent(in) :: map_w3
-  integer(kind=i_def), dimension(ndf_wt), intent(in) :: map_wt
-
-  real(kind=r_def), dimension(undf_w3), intent(inout) :: exner
-  real(kind=r_def), dimension(undf_w3), intent(in)    :: height_w3, &
-                                                         w3_mask,   &
-                                                         phi
-  real(kind=r_def), dimension(undf_wt), intent(in)    :: moist_dyn_gas, &
-                                                         moist_dyn_tot
-  real(kind=r_def), dimension(undf_wt), intent(in)    :: temperature, &
-                                                         height_wth
-  real(kind=r_def), dimension(undf_wt), intent(in)    :: coriolis_term
-  real(kind=r_def),                     intent(in)    :: cp
+  real(kind=r_def), intent(inout) :: exner(undf_w3)
+  real(kind=r_def), intent(in)    :: temperature(undf_wt)
+  real(kind=r_def), intent(in)    :: coriolis_term(undf_wt)
+  real(kind=r_def), intent(in)    :: moist_dyn_gas(undf_wt)
+  real(kind=r_def), intent(in)    :: moist_dyn_tot(undf_wt)
+  real(kind=r_def), intent(in)    :: phi(undf_w3)
+  real(kind=r_def), intent(in)    :: height_w3(undf_w3)
+  real(kind=r_def), intent(in)    :: height_wth(undf_wt)
+  real(kind=r_def), intent(in)    :: mask_w3(undf_w3)
+  real(kind=r_def), intent(in)    :: cp
+  integer(kind=i_def), intent(in) :: map_w3(ndf_w3)
+  integer(kind=i_def), intent(in) :: map_wt(ndf_wt)
 
   ! Internal variables
   integer(kind=i_def) :: k
@@ -121,14 +120,15 @@ subroutine regrav_preserve_t_code( nlayers,       &
 
   ! Return if the mask is 0 (with tolerance of 0.5 as mask is real, 0 or 1)
   ! setting exner to 1
-  if ( w3_mask( map_w3(1) ) < 0.5_r_def ) then
+  if ( mask_w3( map_w3(1) ) < 0.5_r_def ) then
     do k = 0, nlayers-1
       exner(map_w3(1) + k ) = 1.0_r_def
     enddo
     return
   end if
 
-  ! Integrate from surface to top using model geopotential phi
+  ! Find exner by integrating from lowest layer, preserving the value of
+  ! absolute temperature on each layer boundary. Lowest order FE only.
   do k = 1, nlayers-1
 
     temp_virtual = moist_dyn_gas( map_wt(1) + k ) * temperature( map_wt(1) + k ) / &
@@ -138,8 +138,6 @@ subroutine regrav_preserve_t_code( nlayers,       &
     weight1 = ( height_w3( map_w3(1) + k ) - height_wth( map_wt(1) + k ) ) / dz
     weight2 = ( height_wth( map_wt(1) + k ) -  height_w3( map_w3(1) + k - 1 ) ) /dz
 
-    ! Pi_k = Pi_k-1 * ( cp T_k-1/2 - ( (phi_k - phi_k-1) - F_k-1/2 * dz) * (1-w) ) /
-    !                 ( cp T_k-1/2 + ( (phi_k - phi_k-1) - F_k-1/2 * dz) * w )
     exner( map_w3(1) + k ) = exner( map_w3 (1) + k-1 ) * &
       ( cp * temp_virtual - ( phi( map_w3(1) + k ) - phi( map_w3(1) + k - 1 )     &
                             - coriolis_term( map_wt(1) + k ) * dz ) * weight1 ) / &
