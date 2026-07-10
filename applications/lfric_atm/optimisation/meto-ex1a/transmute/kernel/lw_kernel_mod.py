a5a9252849c5dd4a28fac4e38c92b7adfeab8a59
MetBenjaminWent <benjamin.went@metoffice.gov.uk>
2026-07-10 10:07:22 +0100

    Update both socretes kernels to use the explicit parameters, over options. Depricate old methods, and better align script with existing methods

--- applications/lfric_atm/optimisation/meto-ex1a/transmute/kernel/lw_kernel_mod.py
+++ applications/lfric_atm/optimisation/meto-ex1a/transmute/kernel/lw_kernel_mod.py
@@ -17,65 +17,61 @@ from transmute_psytrans.transmute_functions import (
     replace_n_threads,
     first_priv_red_init)
 
-options = {
-    "node-type-check": False,
-    "ignore_dependencies_for": [
-        "aer_lw_absorption",
-        "aer_lw_asymmetry",
-        "aer_lw_scattering",
-        "aer_mix_ratio",
-        "ch4",
-        "cloud_drop_no_conc",
-        "co",
-        "co2",
-        "conv_frozen_fraction",
-        "conv_frozen_mmr",
-        "conv_frozen_number",
-        "conv_liquid_fraction",
-        "conv_liquid_mmr",
-        "cs",
-        "d_mass",
-        "frozen_fraction",
-        "h2",
-        "h2o",
-        "hcn",
-        "he",
-        "layer_heat_capacity",
-        "li",
-        "liquid_fraction",
-        "mcf",
-        "mcl",
-        "n2",
-        "n2o",
-        "n_ice",
-        "na",
-        "nh3",
-        "o2",
-        "o3",
-        "potassium",
-        "pressure_in_wth",
-        "profile_list",
-        "radiative_cloud_fraction",
-        "radiative_conv_fraction",
-        "rand_seed",
-        "rb",
-        "rho_in_wth",
-        "sigma_mi",
-        "sigma_ml",
-        "so2",
-        "sulphuric",
-        "t_layer_boundaries",
-        "temperature_in_wth",
-        "tile_fraction",
-        "tile_lw_albedo",
-        "tile_temperature",
-        "tio",
-        "vo",
-    ],
-}
 
-# Associate ll index with segmentation_ll tool type
-Loop.set_loop_type_inference_rules({"segmentation_ll": {"variable": "ll"}})
+node_type_check = False
+ignore_dependencies_for = [
+    "aer_lw_absorption",
+    "aer_lw_asymmetry",
+    "aer_lw_scattering",
+    "aer_mix_ratio",
+    "ch4",
+    "cloud_drop_no_conc",
+    "co",
+    "co2",
+    "conv_frozen_fraction",
+    "conv_frozen_mmr",
+    "conv_frozen_number",
+    "conv_liquid_fraction",
+    "conv_liquid_mmr",
+    "cs",
+    "d_mass",
+    "frozen_fraction",
+    "h2",
+    "h2o",
+    "hcn",
+    "he",
+    "layer_heat_capacity",
+    "li",
+    "liquid_fraction",
+    "mcf",
+    "mcl",
+    "n2",
+    "n2o",
+    "n_ice",
+    "na",
+    "nh3",
+    "o2",
+    "o3",
+    "potassium",
+    "pressure_in_wth",
+    "profile_list",
+    "radiative_cloud_fraction",
+    "radiative_conv_fraction",
+    "rand_seed",
+    "rb",
+    "rho_in_wth",
+    "sigma_mi",
+    "sigma_ml",
+    "so2",
+    "sulphuric",
+    "t_layer_boundaries",
+    "temperature_in_wth",
+    "tile_fraction",
+    "tile_lw_albedo",
+    "tile_temperature",
+    "tio",
+    "vo",
+]
 
 
 def trans(psyir):
@@ -93,13 +89,13 @@ def trans(psyir):
 
     # Walk the loops of the psyir obj
     for loop in psyir.walk(Loop):
-        # if the loop is of the set type above, segmentation_ll
-        if loop.loop_type == "segmentation_ll":
-            # first private workaround
-            first_priv_red_init(loop, ["n_profile_list_seg", "seg_end"])
-            # Apply the transformation
+        # There is only one loop to parallelise, ll
+        if loop.variable.name == "ll":
             try:
-                OMP_PARALLEL_LOOP_DO_TRANS_STATIC.apply(loop, options)
+                OMP_PARALLEL_LOOP_DO_TRANS_STATIC.apply(
+                    loop,
+                    ignore_dependencies_for=ignore_dependencies_for,
+                    node_type_check=node_type_check)
             except (TransformationError, IndexError) as err:
                 logging.warning(
                     "Could not transform because:\n %s", err)
