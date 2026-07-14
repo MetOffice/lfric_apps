c86ef71bd69b2fbe92d7a76467d90bc5a4fb0b07
James Bruten <109733895+james-bruten-mo@users.noreply.github.com>
2026-07-14 13:14:59 +0100

    tweak for checksums

--- applications/lfric_atm/build/project.mk
+++ applications/lfric_atm/build/project.mk
@@ -1,10 +1,10 @@
 ##############################################################################
-# (c) Crown copyright 2017 Met Office. All rights reserved.
+# (c) Crown copyright 2021 Met Office. All rights reserved.
 # The file LICENCE, distributed with this code, contains details of the terms
 # under which the code may be used.
 ##############################################################################
 
-$(info UM physics project specials)
+$(info Coupling project configuration)
 
 export PRE_PROCESS_INCLUDE_DIRS = \
         $(WORKING_DIR)/atmosphere_service/include \
@@ -13,3 +13,5 @@ export PRE_PROCESS_INCLUDE_DIRS = \
         $(WORKING_DIR)/free_tracers/include
 
 export PRE_PROCESS_MACROS += UM_PHYSICS LFRIC USSPPREC_32B LSPREC_32B UM_JULES
+
+export PRE_PROCESS_MACROS += COUPLED
