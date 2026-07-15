aefc826f80605ab897b9e476390661a2d1c9f104
MetBenjaminWent <benjamin.went@metoffice.gov.uk>
2026-07-15 16:54:36 +0100

    Align copyright

--- applications/lfric_atm/optimisation/meto-ex1a/transmute/science/ukca/src/control/core/top_level/ukca_main1-ukca_main1.py
+++ applications/lfric_atm/optimisation/meto-ex1a/transmute/science/ukca/src/control/core/top_level/ukca_main1-ukca_main1.py
@@ -1,8 +1,8 @@
-##############################################################################
-# (c) Crown copyright 2025 Met Office. All rights reserved.
+# -----------------------------------------------------------------------------
+# (C) 2025 Crown copyright Met Office. All rights reserved.
 # The file LICENCE, distributed with this code, contains details of the terms
 # under which the code may be used.
-##############################################################################
+# -----------------------------------------------------------------------------
 
 # Summary
 # =======
